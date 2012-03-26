4fc3b5481c8e3d12c6309cdd84b532a407df929f
Brian Hicks <brian@brianthicks.com>
2012-03-25 21:59:50 -0500

    add AuthToken model

--- autoauth/models.py
+++ autoauth/models.py
@@ -1,1 +1,27 @@
 'models for autoauth'
+from datetime import datetime, timedelta
+
+from django.conf import settings
+from django.db import models
+
+from hashlib import sha1
+
+class AuthToken(models.Model):
+    'auth token model'
+    expires = models.DateTimeField()
+    used = models.BooleanField(default=False)
+    hash = models.StringField(max_length=40)
+    user = models.ForeignKey('django.contrib.auth.User')
+
+    def save(self, *args, **kwargs):
+        'save this instance'
+        if self.expires is None:
+            offset = getattr(settings, 'AUTOAUTH_EXPIRY', timedelta(days=1))
+            self.expires = datetime.today + offset
+
+        if self.hash is None:
+            self.hash = sha1(
+                self.expires.isoformat() + str(user.pk)
+            ).hexdigest()
+
+        super(AuthToken, self).save(*args, **kwargs)
